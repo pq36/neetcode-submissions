29f6e14603b04fc950d40765c34a5d594a34e651
Meghana M <thanksuniverse369@gmail.com>
2026-08-03 13:53:43 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def f(self,n,dp):
+        if n<=0:
+            return 0
+        if n<=2:
+            return n
+        if dp[n]!=-1:
+            return dp[n]
+        dp[n]=self.f(n-1,dp)+self.f(n-2,dp)
+        return dp[n]
+    def climbStairs(self, n: int) -> int:
+        dp=[-1]*(n+1)
+        return self.f(n,dp)
+        
+        
